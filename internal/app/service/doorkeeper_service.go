265a93ab1c29a9ebfc48a5a787f8739c1d6730ba
rmscoal <rifkymanuelsatyana@gmail.com>
2023-04-19 21:44:15 +0700

    feat: added generate token method

--- internal/app/service/doorkeeper_service.go
+++ internal/app/service/doorkeeper_service.go
@@ -1,6 +1,8 @@
 package service
 
+import "github.com/rmscoal/go-restful-monolith-boilerplate/internal/domain"
+
 type IDoorkeeperService interface {
 	HashPassword(pass string) string
-	// GenerateToken(user domain.User)
+	GenerateToken(user domain.User) (string, error)
 }
