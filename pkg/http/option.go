804328e2f5630a845ec2668e2b0e1fbb02cc1d63
rmscoal <rifkymanuelsatyana@gmail.com>
2023-04-23 19:53:00 +0700

    style: better function naming

--- pkg/http/option.go
+++ pkg/http/option.go
@@ -9,28 +9,28 @@ import (
 type Option func(*Server)
 
 // Port -.
-func Port(port string) Option {
+func RegisterHostAndPort(host, port string) Option {
 	return func(s *Server) {
-		s.server.Addr = net.JoinHostPort("", port)
+		s.server.Addr = net.JoinHostPort(host, port)
 	}
 }
 
 // ReadTimeout -.
-func ReadTimeout(timeout time.Duration) Option {
+func RegisterReadTimeout(timeout time.Duration) Option {
 	return func(s *Server) {
 		s.server.ReadTimeout = timeout
 	}
 }
 
 // WriteTimeout -.
-func WriteTimeout(timeout time.Duration) Option {
+func RegisterWriteTimeout(timeout time.Duration) Option {
 	return func(s *Server) {
 		s.server.WriteTimeout = timeout
 	}
 }
 
 // ShutdownTimeout -.
-func ShutdownTimeout(timeout time.Duration) Option {
+func RegisterShutdownTimeout(timeout time.Duration) Option {
 	return func(s *Server) {
 		s.shutdownTimeout = timeout
 	}
