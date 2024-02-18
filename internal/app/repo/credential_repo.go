9b19f4ec00293b0110790e4203d1573157da450f
Rifky Manuel Satyana <rmscoaldev@gmail.com>
2024-02-18 13:36:23 +0700

    ref: clean up repo adapter

--- internal/app/repo/credential_repo.go
+++ internal/app/repo/credential_repo.go
@@ -1,21 +1,5 @@
 package repo
 
-import (
-	"context"
-
-	"github.com/rmscoal/moilerplate/internal/domain"
-	"github.com/rmscoal/moilerplate/internal/domain/vo"
-)
-
 type ICredentialRepo interface {
 	IBaseRepo
-
-	CreateNewUser(ctx context.Context, user domain.User) (domain.User, error)
-	GetUserByUsername(ctx context.Context, username string) (domain.User, error)
-	GetUserByJti(ctx context.Context, jti string) (domain.User, error)
-	SetNewUserToken(ctx context.Context, user domain.User) (vo.UserToken, error)
-	UndoSetUserToken(ctx context.Context, jti string) error
-	GetLatestUserTokenVersion(ctx context.Context, user domain.User) (int, error)
-	DeleteUserTokenFamily(ctx context.Context, user domain.User) error
-	RotateUserHashPassword(ctx context.Context, user domain.User) error
 }
